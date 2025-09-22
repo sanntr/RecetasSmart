3fd742669848f373cb4e72bcb4b30fbc2a6b9630
sanntr <sanntrv@gmail.com>
2025-09-22 00:38:33 -0500

    chore: añadir modelos al panel admi

--- productos/admin.py
+++ productos/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from . import models
+
+#lista de modelos
+modelos= [models.Producto, models.CompraProducto]
 
 # Register your models here.
+admin.site.register(modelos)
--- recetas/admin.py
+++ recetas/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from . import models
+
+#lista de modelos
+modelos= [models.Receta, models.RecetaProducto, models.HistoricoReceta]
 
 # Register your models here.
+admin.site.register(modelos)
